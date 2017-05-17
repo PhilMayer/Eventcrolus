ddfdbfde89cd16200bc0c8c14d097bb95390f647
PhilMayer <phillipmmayer@gmail.com>
2017-05-17 13:19:00 -0400

    Render fields for creating new event

--- src/App.js
+++ src/App.js
@@ -17,6 +17,54 @@ class App extends Component {
     }
   }
 
+  /*
+  Renders three inputs--a text input for title, and two date inputs for start and end times.
+  If errors are present, they are displayed below the form.
+  */
+  newEvent () {
+    return (
+      <div>
+        <p className="header">Create a new event</p>
+        <div className="event-form">
+          <div>
+            <span className="form-label">Title</span>
+            <input
+              value={this.state.title}
+              onChange={(e) => this.setState({title: e.target.value})}/>
+          </div>
+
+          <div>
+            <span className="form-label">Start Time</span>
+            <input
+              type="date"
+              value={this.state.startTime}
+              onChange={(e) => this.setState({startTime: e.target.value})}/>
+          </div>
+
+          <div>
+            <span className="form-label">End Time</span>
+            <input
+              type="date"
+              value={this.state.endTime}
+              onChange={(e) => this.setState({endTime: e.target.value})}/>
+          </div>
+
+          <button
+            onClick={() => this.handleSubmit()}
+            className="submit-button">CREATE EVENT
+          </button>
+        </div>
+
+        <p className={this.state.titleEmptyError ? "errors" : "hidden"}>
+          Isn't it maybe a little silly to have an event without a title?
+        </p>
+        <p className={this.state.invalidDateError ? "errors" : "hidden"}>
+          Dates are so invalid it's not even funny
+        </p>
+      </div>
+    )
+  }
+
   render() {
     let events;
     if (this.state.events) {
