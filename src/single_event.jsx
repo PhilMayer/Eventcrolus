57a83a3b95ef31aed46e4508f444bbfeb9857959
PhilMayer <phillipmmayer@gmail.com>
2017-05-18 10:00:47 -0400

    Refactor SingleEvent into functional component

--- src/single_event.jsx
+++ src/single_event.jsx
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import dateFormat from 'dateformat';
 
-export default class SingleEvent extends Component {
-  // Renders an event's information.
-  render () {
-    const title = this.props.event.title
-    const startTime = new Date(this.props.event.startTime).toString().slice(0,16)
-    const endTime = new Date(this.props.event.endTime).toString().slice(0,16)
-    const formattedStartTime = dateFormat(startTime, "dddd, mmmm dS, yyyy")
-    const formattedEndTime = dateFormat(endTime, "dddd, mmmm dS, yyyy")
+const SingleEvent = (props) => {
+  const title = props.event.title
+  const startTime = new Date(props.event.startTime).toString().slice(0,16)
+  const endTime = new Date(props.event.endTime).toString().slice(0,16)
+  const formattedStartTime = dateFormat(startTime, "dddd, mmmm dS, yyyy")
+  const formattedEndTime = dateFormat(endTime, "dddd, mmmm dS, yyyy")
 
-    return (
-      <div className="event">
-        <h3>{title}</h3>
+  return (
+    <div className="event">
+      <h3>{title}</h3>
 
-        <div className="dates">
-          <span>{formattedStartTime}</span>
-          <span className={startTime === endTime ? "hidden" : ""}>- {formattedEndTime}</span>
-        </div>
+      <div className="dates">
+        <span>{formattedStartTime}</span>
+        <span className={startTime === endTime ? "hidden" : ""}>- {formattedEndTime}</span>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default SingleEvent;
